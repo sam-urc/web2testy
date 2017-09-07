b399135cd8f686a1eed80cd35ca78866646a3760
Sam <purrlin1@gmail.com>
2017-09-07 17:05:44 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 console.log('heya world!')
 document.write('<h1> Hi!</h1>')
+
+adj = ["rad", "discombobulating", "jazzy", "smooth", "breezy"]
+"Welcome to the "+adj[Math.floor(adj.length*Math.random())]+" shack!"
+my_sentence = "Welcome to the "+adj[Math.floor(adj.length*Math.random())]+" shack! "
+document.write(my_sentence)
+
+verb = ["run", "dance", "bop", "headbang", "jump"]
+"Here you can feel free to "+verb[Math.floor(verb.length*Math.random())]+" whenever you want!"
+my_sentence2 = "Here you can feel free to "+verb[Math.floor(verb.length*Math.random())]+" whenever you want! "
+document.write(my_sentence2)
+
+noun = ["horses", "alligators", "megaladons", "lawnmowers", "mayflies"]
+"You can also go on adventure and take a ride on any one of our "+noun[Math.floor(noun.length*Math.random())]+"!"
+my_sentence3 = "You can also go on adventure and take a ride on any one of our "+noun[Math.floor(noun.length*Math.random())]+"! "
+document.write(my_sentence3)
